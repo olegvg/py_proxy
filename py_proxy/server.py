dd9e3fbd8bf20134f1eb5417a9e0bd31e541aba9
Oleg Gaidukov <ogaidukov@gmail.com>
2017-10-14 01:24:31 +0300

    initial commit

--- py_proxy/server.py
+++ py_proxy/server.py
@@ -25,7 +25,10 @@ class Proxy:
     async def proxy_handler(self, req):
         self.ensure_connector()
 
-        url = req.url.with_host('habrahabr.ru').with_port(None).with_scheme('https')
+        url = req.url.\
+            with_host('habrahabr.ru')\
+            .with_port(None)\
+            .with_scheme('https')
         req.headers.pop('Accept-Encoding', None)
         req.headers.pop('Connection', None)
         req.headers.pop('Host', None)
@@ -43,7 +46,9 @@ class Proxy:
             proxy_headers.pop('Content-Encoding', None)
             proxy_headers.pop('Connection', None)
 
-            proxy_resp = web.StreamResponse(headers=proxy_headers, status=resp.status)
+            proxy_resp = web.StreamResponse(
+
+                headers=proxy_headers, status=resp.status)
 
             for v in resp.cookies.items():
                 proxy_resp.set_cookie(*v)
@@ -51,11 +56,13 @@ class Proxy:
 
             if resp.content_type == 'text/html':
                 proxy_resp.charset = resp.charset
-                content = content.decode(resp.charset if resp.charset else 'UTF-8')
+                content = content.decode(resp.charset
+                                         if resp.charset else 'UTF-8')
 
                 content = self.alter_content(content)
 
-                content = content.encode(resp.charset if resp.charset else 'UTF-8')
+                content = content.encode(resp.charset
+                                         if resp.charset else 'UTF-8')
 
             await proxy_resp.prepare(req)
             proxy_resp.write(content)
@@ -64,7 +71,11 @@ class Proxy:
 
     @staticmethod
     def alter_content(content):
-        content = re.sub(r'https://habrahabr\.ru/', 'http://127.0.0.1:8080/', content, flags=re.S)
+        content = re.sub(
+            r'https://habrahabr\.ru/',
+            'http://127.0.0.1:8080/',
+            content,
+            flags=re.S)
 
         tree = html.fromstring(content)
 
--- setup.py
+++ setup.py
@@ -1,2 +1,27 @@
-# -*- coding: utf-8 -*-
+from pip.req import parse_requirements
+from setuptools import setup
 
+reqs = [str(ir.req) for ir in
+        parse_requirements('requirements.txt', session='')]
+
+tests_require = [
+]
+
+setup(
+    name='py_proxy',
+    version='0.1',
+    packages=['py_proxy'],
+    test_suite='testsuite',
+    install_requires=reqs,
+    entry_points={
+        "console_scripts": [
+            "py_proxy=py_proxy:main"
+        ]
+    },
+    setup_requires=[
+        "flake8"
+    ],
+    zip_safe=True,
+    tests_require=tests_require,
+    python_requires='>=3.5'
+)
